485e635c3d283a43faf6b278313c96558b373589
JinmingWan <109368823+JinmingWan@users.noreply.github.com>
2025-05-12 02:30:55 -0400

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import yaml, random, numpy as np, torch
+
+def load_config(path="src/config.yaml"):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
